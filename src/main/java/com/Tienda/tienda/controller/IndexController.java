69af77b3826ca9cb4ac8b5e2b9dc73d3ad3681de
Funny <dquesadaa2004@gmail.com>
2024-10-15 14:00:07 -0600

    Practica Semana 4

---
+++ src/main/java/com/Tienda/tienda/controller/IndexController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
+ */
+package com.Tienda.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author dques
+ */
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String paginaInicio(Model model) {
+        //model.addAttribute("attribute", "value");
+        return "index";
+    }
+    
+    @RequestMapping("/informacion")
+    public String paginaInfo(Model model) {
+        //model.addAttribute("attribute", "value");
+        return "informacion";
+    }
+    
+}
