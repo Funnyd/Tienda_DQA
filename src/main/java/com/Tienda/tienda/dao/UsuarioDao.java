a61f5552d2da35b0e9fd3fbd11dae9651974f965
Funny <dquesadaa2004@gmail.com>
2024-12-04 10:39:14 -0600

    Finalizacion Sem 11

--- src/main/java/com/Tienda/tienda/dao/UsuarioDao.java
+++ src/main/java/com/Tienda/tienda/dao/UsuarioDao.java
@@ -2,9 +2,14 @@
 package com.Tienda.tienda.dao;
 import com.Tienda.tienda.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
+
+public interface UsuarioDao extends JpaRepository<Usuario, Long> {
     Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
 }
